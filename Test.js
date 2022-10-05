8025c7b33fa8d69b1e9acbcbef5139da0c87eded
dedi <dedi>
2022-10-05 23:14:45 +0700

    setup

---
+++ Test.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class Test extends Component {
+  render() {
+    const cob = 'Nama saya';
+    return (
+      <div>
+        <Text>Hello Wolrd!</Text>
+      </div>
+    );
+  }
+}
+
+export default Test;
