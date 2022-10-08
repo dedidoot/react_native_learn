c3f78d3f2bfbdd21252e7cc6a49c76695d766b6c
dedi <dedi>
2022-10-08 23:58:42 +0700

    styling product component

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import {
+  Button,
   Dimensions,
   Image,
   SafeAreaView,
+  ScrollView,
+  StyleSheet,
   Text,
   TextInput,
   View,
 } from 'react-native';
 import {Component} from 'react';
+import shoes from './assets/ic_shoes.jpeg';
 
 const App = () => {
+  return (
+    <ScrollView style={{backgroundColor: 'white'}}>
+      <ProductComponent />
+      <ProductComponent />
+      <ProductComponent />
+      <ProductComponent />
+      <ProductComponent />
+      <ProductComponent />
+      <ProductComponent />
+      <ProductComponent />
+      <ProductComponent />
+    </ScrollView>
+  );
+};
+
+const SampleComponent = () => {
   function onText(text) {
     console.log(text);
   }
@@ -55,23 +75,76 @@ const MyPhoto = () => {
 class ProductComponent extends Component {
   render() {
     return (
-      <View>
+      <View
+        style={{
+          backgroundColor: '#f2f2f2',
+          padding: 10,
+          width: 212,
+          marginTop: 16,
+          marginLeft: 16,
+          borderRadius: 8,
+        }}>
         <Image
-          source={{
-            uri: 'https://images.unsplash.com/photo-1560769629-975ec94e6a86',
-          }}
+          source={shoes}
           style={{
-            width: SCREEN_WIDTH / 2,
-            height: SCREEN_HEIGHT / 4,
+            width: 188,
+            height: 107,
             borderRadius: 10,
-            alignSelf: 'center',
           }}
         />
-        <Text style={{textAlign: 'center'}}>Shoes</Text>
+        <Text style={TitleTextStyle.text}>Shoes</Text>
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: 'bold',
+            color: 'orange',
+            marginTop: 12,
+          }}>
+          Rp. 280.000
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: '300',
+            color: 'black',
+            marginTop: 4,
+          }}>
+          Jakarta
+        </Text>
+        <BuildButton />
       </View>
     );
   }
 }
+
+const BuildButton = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'green',
+        borderRadius: 12,
+        marginTop: 12,
+      }}>
+      <Text
+        style={{
+          color: 'white',
+          textAlign: 'center',
+          fontWeight: 'bold',
+          padding: 8,
+        }}>
+        Buy
+      </Text>
+    </View>
+  );
+};
+
+const TitleTextStyle = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 16,
+  },
+});
 
 export const SCREEN_WIDTH = Dimensions.get('window').width;
 export const SCREEN_HEIGHT = Dimensions.get('window').height;
